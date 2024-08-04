501e3fdd69cdd9bec09fa9a7cc38cb329f6bf9c7
Edmel John Linaugo <68092712+EdmelKun@users.noreply.github.com>
2024-08-04 20:06:34 +0800

    chore: update server port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    host: true,
+    strictPort: true,
+    port: 8000,
   },
 });
