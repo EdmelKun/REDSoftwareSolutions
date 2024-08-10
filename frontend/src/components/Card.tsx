18a32c9716d30c88244ed5eec488daf242c4617e
Edmel John Linaugo <68092712+EdmelKun@users.noreply.github.com>
2024-08-10 12:28:08 +0800

    chore: change int parsing

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -57,7 +57,7 @@ const CardComponent = ({
         {show && (
           <div className="absolute top-0 left-0 w-full h-full z-0 flex justify-center items-center">
             <Lottie
-              animationData={animations[Number(custom)]}
+              animationData={animations[parseInt(custom)]}
               loop={false}
               autoplay={true}
               style={{ pointerEvents: "none", width: "70%", height: "70%" }}
